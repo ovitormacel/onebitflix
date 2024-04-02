c18d05d0fd1d7253f3ed554a068636fabefd0e35
ovitormacel <ovitormacel00@gmail.com>
2024-04-02 11:48:30 -0300

    Autenticação AdminJS

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -3,6 +3,9 @@ import AdminJSExpress from "@adminjs/express";
 import AdminJSSequelize from "@adminjs/sequelize";
 import { sequelize } from "../database";
 import { adminJsResources } from "./resources";
+import { User } from "../models";
+import bcrypt from "bcrypt";
+import { locale } from "./locale";
 
 //Utiliza o ORM Sequelize
 AdminJS.registerAdapter(AdminJSSequelize);
@@ -32,8 +35,30 @@ export const adminJs = new AdminJS({
                 hoverBg: '#151515',
             }
         }
-    }
+    },
+    locale: locale
 });
 
 //Cria as rotas necessárias. Utilizado como um Middleware
-export const adminJsRouter = AdminJSExpress.buildRouter(adminJs);
+export const adminJsRouter = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
+    // Solicita uma autenticação para as Rotas
+    authenticate: async (email, password) => {
+        const user = await User.findOne({ where: { email: email } });
+        
+        if (user && user.role === "admin") {
+            const matched = await bcrypt.compare(password, user.password);
+        
+            if(matched) {
+                // AUTENTICADO
+                return user;
+            }
+        }
+
+        // NÃO AUTENTICADO
+        return false;
+    },
+    cookiePassword: "123456"
+}, null, {
+    resave: false,
+    saveUnitialized: false
+});
