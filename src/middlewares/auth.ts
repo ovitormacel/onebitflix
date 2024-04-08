8ece5fb1cf9475020950eb326e0972edf5c3ebe8
ovitormacel <ovitormacel00@gmail.com>
2024-04-08 19:16:49 -0300

    Proteção das Rotas de Videos

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,8 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 import { jwtService } from "../services/jwtService";
 import { userService } from "../services/userService";
-import { JwtPayload } from "jsonwebtoken";
+import { Jwt, JwtPayload } from "jsonwebtoken";
 import { UserInstance } from "../models/User";
+import { decode } from "punycode";
 
 export interface AuthenticatedRequest extends Request {
     user?: UserInstance | null
@@ -16,12 +17,29 @@ export function ensureAuth(req: AuthenticatedRequest, res: Response, next: NextF
     const token = authorizationHeader.replace(/Bearer /, '');
 
     // Se o token for válido, encontra o Usuário no DB e retorna na request.
-    jwtService.verifyToken(token, (error, decoded) => {
+    jwtService.verifyToken(token, async (error, decoded) => {
         if(error || typeof decoded === 'undefined') return res.status(401).json({message: "Token Inválido."});
 
-        userService.findUserByEmail((decoded as JwtPayload).email).then(user => {
-            req.user = user;
-            next();
-        })
+        const user = await userService.findUserByEmail((decoded as JwtPayload).email);
+
+        req.user = user;
+        next();
     })
+}
+
+export function ensureAuthInQuery(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+    const {token} = req.query;
+
+    if(!token) return res.status(401).json({message: "Não autorizado. Nenhum token encontrado."});
+    if(typeof token !== 'string') return res.status(401).json({message: "Parâmetro token deve ser string."});
+
+    jwtService.verifyToken(token, async (error, decoded) => {
+        if(error || typeof decoded === 'undefined') return res.status(401).json({message: "Token Inválido."});
+
+        const user = await userService.findUserByEmail((decoded as JwtPayload).email);
+    
+        req.user = user;
+        next();
+    })
+
 }
