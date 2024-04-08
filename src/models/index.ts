536cd52f7c7be4d1ae251ae07276fff2e873e20b
ovitormacel <ovitormacel00@gmail.com>
2024-04-08 20:03:02 -0300

    Adicionar cursos aos favoritos

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,6 +2,7 @@ import { Category } from "./Category";
 import { Course } from "./Course";
 import { Episode } from "./Episode";
 import { User } from "./User";
+import { Favorite } from "./Favorite";
 
 //Associações
 Category.hasMany(Course, {as: 'courses'});
@@ -10,9 +11,19 @@ Course.belongsTo(Category);
 Course.hasMany(Episode, {as: 'episodes'});
 Episode.belongsTo(Course);
 
+Course.belongsToMany(User, { through: Favorite });
+User.belongsToMany(Course, { through: Favorite });
+
+Course.hasMany(Favorite, { as: 'FavoritesUsers', foreignKey: 'course_id' });
+Favorite.belongsTo(Course);
+User.hasMany(Favorite, { as: 'FavoritesCourses', foreignKey: 'user_id' });
+Favorite.belongsTo(User);
+
+
 export {
     Category,
     Course,
     Episode,
-    User
+    User,
+    Favorite
 }
--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { coursesController } from "./controllers/coursesController";
 import { episodesController } from "./controllers/episodesController";
 import { authController } from "./controllers/authController";
 import { ensureAuth, ensureAuthInQuery } from "./middlewares/auth";
+import { favoriteController } from "./controllers/favoriteController";
 
 const router = express.Router();
 
@@ -24,5 +25,8 @@ router.get("/courses/:id", ensureAuth, coursesController.show);
 
 // Episodes
 router.get("/episodes/stream", ensureAuthInQuery, episodesController.stream);
+
+// Favorites
+router.post("/favorites", ensureAuth, favoriteController.save);
 
 export {router};
